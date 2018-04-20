0229105882c3c3db325478b8113f6768a955c650
github username <github email address>
2018-04-20 11:06:32 -0400

    Done.

--- redux-pattern.js
+++ redux-pattern.js
@@ -18,17 +18,6 @@ function createStore(reducer) {
   };
 };
 
-function changeCount(state = {
-  count: 0,
-}, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return { count: state.count + 1 };
-    default:
-      return state;
-  };
-};
-
 
 function render(){
   let container = document.getElementById('container');
